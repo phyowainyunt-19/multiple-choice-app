85fd050a991481338bb1f0ffd0a8920eae5978f9
PhyoWaiNyunt <50352633+pwn-69@users.noreply.github.com>
2021-10-14 13:45:31 +0630

    fixed demo

--- src/router/index.js
+++ src/router/index.js
@@ -14,50 +14,9 @@ const routes = [{
         component: Home,
         meta: { requiresAuth: false }
     },
-    // {
-    //     path: '*',
-    //     redirect: '/login'
-    // },
-    // {
-    //     path: '/login',
-    //     redirect: '/login'
-    // },
-    // {
-    //     path: '/login',
-    //     name: 'Login',
-    //     component: Login
-    // },
-    // {
-    //     path: '/signup',
-    //     name: 'Signup',
-    //     component: Signup
-    // },
-
-    // {
-    //     name: 'Add',
-    //     path: '/add',
-    //     component: Add
-    // },
-    // {
-    //     name: 'ViewAll',
-    //     path: '/search',
-    //     component: Search
-    // },
-    // {
-    //     name: 'Admin',
-    //     path: '/admin',
-    //     component: Admin
-    // }
 ]
 const
     router = new VueRouter({
         routes
     })
-    // router.beforeEach((to, from, next) => {
-    //     const currentUser = Firebase.auth().currentUser;
-    //     const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-    //     if (requiresAuth && !currentUser) next('/login');
-    //     else if (!requiresAuth && currentUser) next('/home');
-    //     else next();
-    // });
 export default router;
